aae35350dc282b4fd8192b094e4099fd4642ad24
Amruta Pede <121037776+Amruta20pede@users.noreply.github.com>
2023-07-29 16:30:29 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export const getServerSideProps = async (ctx) => {
     admin = true;
   }
 
-  const res = await axios.get("http://localhost:3000/api/products");
+  const res = await axios.get("https://pizza-pizza-deploy.vercel.app/api/products");
   return {
     props: {
       pizzaList: res.data,
