2bc264c65dfe94f9c75b8bd6c8f862c7a86b12bd
Amruta Pede <121037776+Amruta20pede@users.noreply.github.com>
2023-07-29 16:28:07 +0530

    Update [id].jsx

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -118,7 +118,7 @@ const Order = ({ order }) => {
 
 export const getServerSideProps = async ({ params }) => {
   const res = await axios.get(
-    `http://localhost:3000/api/orders/${params.id}`
+    `https://pizza-pizza-deploy.vercel.app/api/orders/${params.id}`
   );
   return {
     props: { order: res.data },
